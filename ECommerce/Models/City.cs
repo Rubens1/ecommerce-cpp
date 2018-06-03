a86e6b0ff7bdc8b13e1bc768b9a1db1b72278714
rubens.jesus123 <rubens.jesus1997@gmail.com>
2018-06-02 21:41:08 -0300

    Departments and Cities basic CRUD

---
+++ ECommerce/Models/City.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Models
+{
+    public class City
+    {
+        [Key]
+        public int CityId { get; set; }
+
+        [Required(ErrorMessage = " O campo {0} é requirido")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres de completo")]
+        [Display(Name = "Cidade")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = " O campo {0} é requirido")]
+        public int DepartmentId { get; set; }
+
+        public virtual Department Department { get; set; }
+    }
+}
---
+++ ECommerce/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        [Required(ErrorMessage =" O campo {0} é requirido")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres de completo")]
+        [Display (Name ="Departamento")]
+        public string Name { get; set; }
+
+        public virtual ICollection<City> Cities { get; set; }
+    }
+}
